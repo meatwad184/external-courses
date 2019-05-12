c545f4579a80911bf247193cb038ca36ebb129f6
meatwad184 <loh20000000@yandex.com>
2019-05-12 18:10:38 +0300

    made some functions-analog methods for arrays

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -1,22 +1,19 @@
-function sliceMethodAnalog(array, begin, end){
-	var copyArray = [];
-	var start = begin;
-	var stop = end;
-	if(!start && !stop){
-		copyArray = array
-	} 
-	for(var i = 0; i < array.length; i++){
-		if (start < 0 && start !== stop){
-			copyArray.push(array[array.length + start]);
-			start++;
-		} else if (!stop && stop < array.length){
-			copyArray.push(array[start]);
-			start++;
-		} else if (start < stop && start < array.length) {
-			copyArray.push(array[start]);
-			start++;
-		}
+function sliceMethodAnalog(arr, begin, end) {
+	var newArr = [];
+	var start, stop;
+	if (!begin) {
+		start = 0;
+	} else {
+		start = begin;
 	}
-	return copyArray;
+	if (!end || end >= arr.length) {
+		stop = arr.length;
+	} else stop = end;
+	if (begin < 0) stop += arr.length;
+	if (end < 0) stop += arr.length;
+	for (var i = start; i < stop; i++) {
+		newArr.push(arr[i]);
+	}
+	return newArr;
 }
 module.exports = sliceMethodAnalog;
